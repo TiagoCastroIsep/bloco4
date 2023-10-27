65321e0d575c947cf492a5c0f054b10cb7e3eda2
TiagoCastro <1060598@isep.ipp.pt>
2023-10-27 09:37:07 +0100

    Bloco11 finished

--- src/main/java/org/example/Bloco4Ex11.java
+++ src/main/java/org/example/Bloco4Ex11.java
@@ -1,8 +1,7 @@
 package org.example;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Stream;
 
 public class Bloco4Ex11 {
 
@@ -10,7 +9,7 @@ public class Bloco4Ex11 {
         int a = 4;
         int b = 12;
         int[] multiplesOf = {2, 3};
-        System.out.println(Arrays.toString(getMultiplesOfNumbersInRange(a, b, multiplesOf)));
+        System.out.println("Result with normal arrays: " + Arrays.toString(getMultiplesOfNumbersInRange(a, b, multiplesOf)));
     }
 
     public static int[] getMultiplesOfNumbersInRange(int a, int b, int[] numbers) {
@@ -21,27 +20,14 @@ public class Bloco4Ex11 {
         int minLimit = limits[0];
         int maxLimit = limits[1];
 
-        int arrayLength = getArrayLengthForCommonMultiples(minLimit, maxLimit, numbers);
+        /** using ArrayList to avoid getting the arrayLength beforehand or cleanup of the array afterward **/
+        findCommonMultiplesWithArrayList(numbers, minLimit, maxLimit);
+        int arrayLength = getArrayLengthForCommonMultiples(numbers, minLimit, maxLimit);
 
         int[] commonMultiples = new int[arrayLength];
-//        findCommonMultiplesSet(numbers, minLimit, maxLimit, commonMultiples);
         findCommonMultiples(numbers, minLimit, maxLimit, commonMultiples);
 
         return commonMultiples;
-    }
-
-    private static void findCommonMultiplesSet(int[] numbers, int minLimit, int maxLimit, int[] commonMultiples) {
-        Set<Integer> list = new HashSet<>();
-
-        for (int i = minLimit; i <= maxLimit; i++) {
-            for (int j = 0; j < numbers.length - 1; j++) {
-                if (i % numbers[j] == 0 && i % numbers[j + 1] == 0) {
-                    list.add(i);
-                }
-            }
-        }
-
-        System.out.println(Arrays.toString(list.toArray()));
     }
 
     private static void findCommonMultiples(int[] numbers, int minLimit, int maxLimit, int[] commonMultiples) {
@@ -62,7 +48,24 @@ public class Bloco4Ex11 {
         }
     }
 
-    private static int getArrayLengthForCommonMultiples(int minLimit, int maxLimit, int[] numbers) {
+    private static void findCommonMultiplesWithArrayList(int[] numbers, int minLimit, int maxLimit) {
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = minLimit; i <= maxLimit; i++) {
+            int count = 0;
+
+            for (int number : numbers) {
+                if (i % number == 0) {
+                    count++;
+                }
+            }
+            if (count == numbers.length) list.add(i);
+        }
+
+        System.out.println("Result with ArrayList: " + list);
+    }
+
+    private static int getArrayLengthForCommonMultiples(int[] numbers, int minLimit, int maxLimit) {
         int k = 0;
 
         for (int i = minLimit; i <= maxLimit; i++) {
