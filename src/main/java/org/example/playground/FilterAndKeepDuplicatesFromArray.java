c7346d2db1604a5bb076f66e60acd98e4583cfd6
TiagoCastro <1060598@isep.ipp.pt>
2023-11-01 20:07:40 +0000

    FilterAndKeepDuplicatesFromArray comments added

--- src/main/java/org/example/playground/FilterAndKeepDuplicatesFromArray.java
+++ src/main/java/org/example/playground/FilterAndKeepDuplicatesFromArray.java
@@ -9,10 +9,13 @@ public class FilterAndKeepDuplicatesFromArray {
         int[] onlyDuplicates = new int[2];
 
         int duplicatesIndex = 0;
+        // using 2 pointers to check the i number against every other number for equality.
         for (int i = 0; i < originalArray.length; i++)
             for (int j = i + 1; j < originalArray.length; j++)
                 if (originalArray[i] == originalArray[j]) {
+                    // if the number is already in the duplicates array, break out of this inner loop
                     if (numberIsPresent(onlyDuplicates, originalArray[i])) break;
+                    // otherwise add the number, increase the array index and break out of this inner loop
                     onlyDuplicates[duplicatesIndex] = originalArray[i];
                     duplicatesIndex++;
                     break;
