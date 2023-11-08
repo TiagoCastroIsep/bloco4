4dd0a8bc63b8bd546c34cfab19634775c9f137de
TiagoCastro <1060598@isep.ipp.pt>
2023-11-08 17:10:49 +0000

    Recursion without using global variable

--- src/main/java/org/example/playground/SumAllElementsOfMatrixWithOneForLoop.java
+++ src/main/java/org/example/playground/SumAllElementsOfMatrixWithOneForLoop.java
@@ -9,8 +9,11 @@ public class SumAllElementsOfMatrixWithOneForLoop {
                 {4, 5, 6},
                 {7, 8, 9}
         };
+        int sum1 = 0;
         sumOfMatrixElementsInLineN(matrix, matrix.length - 1);
         System.out.println(sum);
+
+        System.out.println(getSumMatrixElementsInLineN(matrix, matrix.length - 1, sum1));
     }
 
     private static void sumOfMatrixElementsInLineN(int[][] matrix, int lineIndex) {
@@ -20,5 +23,16 @@ public class SumAllElementsOfMatrixWithOneForLoop {
             }
             sumOfMatrixElementsInLineN(matrix, lineIndex - 1);
         }
+    }
+
+    // without using global variable
+    private static int getSumMatrixElementsInLineN(int[][] matrix, int lineIndex, int sum) {
+        if (lineIndex >= 0) {
+            for (int i = 0; i < matrix.length; i++) {
+                sum += matrix[lineIndex][i];
+            }
+            return getSumMatrixElementsInLineN(matrix, lineIndex - 1, sum);
+        }
+        return sum;
     }
 }
